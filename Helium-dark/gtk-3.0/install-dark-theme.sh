2cd7c9545fe2d2738699f7755e1f7ac97c08384f
Lleyton Gray <lea@fyralabs.com>
2022-05-29 23:28:22 -0700

    Use relative option?

--- Helium-dark/gtk-3.0/install-dark-theme.sh
+++ Helium-dark/gtk-3.0/install-dark-theme.sh
@@ -7,8 +7,8 @@ project_name="$2"
 install -m755 -d "${theme_dir}"
 for ver in gtk-3.0; do
   install -m755 -d "${theme_dir}/${ver}"
-  ln -sf "../../${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk.css"
-  ln -sf "../../${project_name}/${ver}/assets" "${theme_dir}/${ver}/assets"
+  ln -sfr "${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk.css"
+  ln -sfr "${project_name}/${ver}/assets" "${theme_dir}/${ver}/assets"
   # For the new dark theme switcher in GNOME 42+
   cp -r "${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk-dark.css"
 done
